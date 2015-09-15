84d9594637c64a42fdeb0a58629f0789764004a8
jserodio <zayk.core@gmail.com>
2015-09-15 14:00:28 +0200

    He añadido la función esSeparable2 dentro de Separable. He cambiado la línea de ejecución de la prueba a esSeparable2().

--- ikasleak/src/EsSeparable/Separable.java
+++ ikasleak/src/EsSeparable/Separable.java
@@ -20,18 +20,20 @@ public class Separable{
     } 
    
     public int esSeparable2(int[] tabla) {
-    	int izq, der;
+    	int izq, der, i;
+    	//der es el total acumulado de la suma desde el final del array
+    	//izq es el total acumulado que suma desde el principio del array
+    	izq = 0;
+    	der = 0;
+    	i = 0;
     	
-
-
-
-
-
-
-
-
-
-
-        return 0;
+    	while (izq != der || i != tabla.length){
+    	// Recorrer toda la tabla mientras las sumas no sean iguales
+    		izq = izq + tabla[i];
+    		//no me funciona por tener valores negativos...
+    		der = der + tabla[tabla.length-i];
+    		i++;
+    	}
+        return i;
     } 
 }
