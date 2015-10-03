290b6870f388ab2139157fe737b599259b83d2c7
jserodio <zayk.core@gmail.com>
2015-10-04 00:45:24 +0200

    LEEDME (doble click): Cambios significantes en el menu.java
    -Cada case ya esta ordenado según el menú.
    -Añadido el default
    -Ahora se sale con el num=9, y así evitamos la asignación num=0
    -He añadido comentarios
    -Más comentarios
    -Un par de comentarios más. Leedlos, hay cosas pendientes!!
    -Al guardar datos (CASE 8) hay dos bucles (coste elevado..)
    deberíamos hablar sobre ello!

--- Labo1/Labo1/src/labo1/Menu.java
+++ Labo1/Labo1/src/labo1/Menu.java
@@ -13,11 +13,17 @@ import java.util.Scanner;
 
 public class Menu {
 
+	static int peliculas;
+	static String reparto;
 	static Actor unActor = null;
 	static String pNombre;
 	static int num;
 
 	public static void main(String[] args) throws IOException {
+		
+		do { // Repetimos el bucle si (num != 0) 
+		
+		// Imprimir por pantalla el menu de usuario
 		System.out.println("¡Bienvenido usuario! Cargando datos...");
 		CatalogoActores.getCatalogoActores().CargarDatosFichero();
 		System.out.println("\nIntroduzca la opcion deseada\n");
@@ -29,105 +35,109 @@ public class Menu {
 		System.out.println("> Opcion 6- Obtener una lista de actores ordenada");
 		System.out.println("> Opcion 7- Hacer una donacion a una pelicula");
 		System.out.println("> Opcion 8- Guardar datos de peliculas a un archivo");
-		System.out.println("> Opcion 9- salir del menu. ");
+		System.out.println("> Opcion 9- Salir del menu");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String Linea = br.readLine();
 		num = Integer.parseInt(Linea);
-
-		while (num != 0) {
-
+		
 			switch (num) {
 
-			case 1:
+			case 1: //buscar actor/actriz
 				System.out.println("Introduzca un nombre a buscar:");
 				pNombre = br.readLine();
-				unActor = CatalogoActores.getCatalogoActores().la
-						.buscarActor(pNombre);
-				// System.out.println(unActor.getNombre()); prueba
-				if (unActor == null) {
-					System.out.println("El actor "+pNombre+" no está."); // ME HE QUEDADO AQUIII
+				unActor = CatalogoActores.getCatalogoActores().la.buscarActor(pNombre);
+				if (unActor == null){
+					System.out.println("El actor " + pNombre + " no está.");
 				} else {
-					System.out.println(unActor.getNombre()
-							+ " esta en la lista.");
+					System.out.println(unActor.getNombre() + " esta en la lista.");
 				}
 				break;
 
-			case 2:
+			case 2: //Insertar actor/actriz
 				System.out.println("Introduce Actor a insertar:");
 				pNombre = br.readLine();
 				unActor = new Actor(pNombre);
-				if (CatalogoActores.getCatalogoActores().getLista()
-						.estaActor(pNombre)) {
+				if (CatalogoActores.getCatalogoActores().getLista().estaActor(pNombre)) {
 					System.out.println("El actor ya esta introducido.");
 				} else {
 					System.out.println("Se va a insertar el actor " + pNombre);
-					CatalogoActores.getCatalogoActores().getLista()
-							.insercionActor(unActor);
+					CatalogoActores.getCatalogoActores().getLista().insercionActor(unActor);
 				}
 				break;
 
-			case 3:
+			case 3: //Borrar un actor/actriz
 				System.out.println("introduce un actor a borrar");
 				pNombre = br.readLine();
-				unActor = CatalogoActores.getCatalogoActores().getLista()
-						.buscarActor(pNombre);
+				unActor = CatalogoActores.getCatalogoActores().getLista().buscarActor(pNombre);
 				if (unActor != null) {
 					System.out.println("el actor esta y se va a borrar");
-					CatalogoActores.getCatalogoActores().getLista()
-							.borrarActor(unActor);
+					CatalogoActores.getCatalogoActores().getLista().borrarActor(unActor);
 				} else {
 					System.out.println("el actor " + pNombre + " no esta");
 				}
 				break;
 
-			case 4:
-
-				System.out
-						.println("Introduce un actor para saber su numero de peliculas:");
+			case 4: //Mostrar peliculas de un actor/actriz
+				System.out.println("Introduzca el nombre del actor:");
 				pNombre = br.readLine();
-				unActor = CatalogoActores.getCatalogoActores().getLista()
-						.buscarActor(pNombre);
+				unActor = CatalogoActores.getCatalogoActores().getLista().buscarActor(pNombre);
 				if (unActor == null) {
 					System.out.println("El actor que buscas no esta.");
 				} else {
-					int peliculas = unActor.devolverNumPelis();
-					System.out.println("El actor que buscas tiene " + peliculas
-							+ " pelicula(s).");
+					peliculas = unActor.devolverNumPelis();
+					System.out.println("El actor que buscas tiene " + peliculas	+ " pelicula(s).");
 				}
-
 				break;
 				
-			case 5: {
-				PrintWriter writer = new PrintWriter(
-						"src/fichero/copiaLista2.txt");
-				Iterator<Actor> it = CatalogoActores.getCatalogoActores()
-						.getLista().getIterador();
-				while (it.hasNext()) {
-					Actor UnActor = it.next();
-					String linea = UnActor.getNombre();
-					Iterator<Pelicula> itr = UnActor.getListaPelis()
-							.getIterador();
-					while (itr.hasNext()) {
-						linea = linea + " \\ " + itr.next().getTitulo();
-					}
-					writer.println(linea);
-				}
-				writer.close();
-
-			}
-				System.out.println("Se a guardar el nuevo catalogo en un txt.");
+			case 5: //Mostrar reparto de una pelicula
+				System.out.println("Introduzca el nombre de la pelicula:");
+				pNombre = br.readLine();
+				//reparto = CatalogoPelis.getCatalogoPelis();
+				//???? reparto = CatalogoPelis.getCatalogoPelis().listaPeliCadaActor();
+				//Tengo que mirar como se hacia para que el mismo metodo fuera diferente
+				//con diferentes parametros en la entrada... polimorfism? o algo asi?           <---------------------PENDIENTE
 				break;
-			case 6:
+				
+			case 6: //Obtener una lista de actores ordenada
 				System.out.println("Se va a ordenar la lista.");
 				CatalogoActores.getCatalogoActores().la.ordenarActores();
 				break;
 
-			case 7:
-				System.out.println("El programa se ha cerrado, adios :)");
-				num = 0;
+			case 7: //Hacer una donacion a una pelicula 
+				System.out.println("Introduzca el nombre de la pelicula:");
+				pNombre = br.readLine();
+				//																				<---------------------PENDIENTE
+				break;
+		
+			case 8: //Guardar datos de peliculas a un archivo
+				PrintWriter writer = new PrintWriter("src/fichero/copiaLista2.txt");
+				Iterator<Actor> it = CatalogoActores.getCatalogoActores().getLista().getIterador();
+				while (it.hasNext()) {
+					Actor UnActor = it.next();
+					String linea = UnActor.getNombre();
+					Iterator<Pelicula> itr = UnActor.getListaPelis().getIterador();
+					while (itr.hasNext()) { //Posibilidad de sacar este while fuera y optimizar. ? idea.
+						linea = linea + " \\ " + itr.next().getTitulo();
+					}
+					writer.println(linea);
+				}
+				writer.close();			
+				System.out.println("Se a guardar el nuevo catalogo en un txt.");
+				break;
+				
+			case 9: //Salir del menu
+				System.out.println("Saliendo del programa... adios! :)");
 				System.exit(num);
+				break;
+			
+			default: //Si se confunde
+				System.out.println("Hmmm ha introducido un valor correcto?");
+				System.out.println("Recuerde! Sólo valores del 1 al 9.");
+				break;
 			}
-
-		}
+			//Fin del switch case
+		}while (num != 9);
 	}
+	//Fin del main
 }
+//Fin menu.java
